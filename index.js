b22adf181fe665965c735a11692c39e50dbfa94d
Michael Lustig <lustig.dev@gmail.com>
2018-07-15 20:26:13 -0400

    Formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import App from "./App";
 
-AppRegistry.registerComponent('BreedUp', () => App);
+AppRegistry.registerComponent("BreedUp", () => App);
