240b8688b30dc580012ad1cf5f253bd0a1296268
Michael Lustig <lustig.dev@gmail.com>
2018-07-16 19:36:24 -0400

    Reorganize App

---
+++ src/Containers/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import RootStack from "../Navigation/AppNavigation";
+
+class App extends Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+export default App;
