451fb69665545ef9287adcf54f667970a2a8691b
Michael Lustig <lustig.dev@gmail.com>
2018-07-22 19:22:44 -0400

    Add react-native-element button back to Login and Signup

--- src/Containers/SignUp/index.js
+++ src/Containers/SignUp/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Text, TextInput, View, Alert, Button } from 'react-native'
-// import { Button } from 'react-native-elements'
+import { Text, TextInput, View, Alert } from 'react-native'
+import { Button } from 'react-native-elements'
 import firebase from 'react-native-firebase'
 
 import styles from './SignUpStyles'
