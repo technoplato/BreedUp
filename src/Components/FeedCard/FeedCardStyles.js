a02b032640221ca47345172e59088be094e1da94
Michael Lustig <lustig.dev@gmail.com>
2018-07-25 18:47:36 -0400

    Add FeedCard dumb component and styles

---
+++ src/Components/FeedCard/FeedCardStyles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    padding: 0
+  },
+  headerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  imageAndTextContainer: {
+    padding: 16,
+    flex: 1,
+    flexDirection: 'row'
+  },
+  postMetadata: {
+    paddingLeft: 12,
+    flexDirection: 'column'
+  },
+  moreButton: {
+    marginRight: 6
+  },
+  text: {
+    paddingRight: 12,
+    paddingBottom: 12,
+    paddingLeft: 12
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 4 / 3
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    backgroundColor: 'transparent'
+  },
+  buttonText: {
+    color: 'black'
+  }
+})
