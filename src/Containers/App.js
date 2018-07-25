f08cc1293f0fd811ee7277a9fed9bf80b537be52
Michael Lustig <lustig.dev@gmail.com>
2018-07-25 18:47:17 -0400

    Configure moment

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import RootStack from '../Navigation/AppNavigation'
+import moment from 'moment'
 
 class App extends Component {
   render() {
+    // Updates moment relative time, not sure where to call this
+    // https://momentjs.com/docs/#/customization/relative-time/
+    moment.updateLocale('en', {
+      relativeTime: {
+        ss: '%ds',
+        s: '%ds',
+        m: '%dm',
+        mm: '%dm',
+        h: '%dh',
+        hh: '%dh',
+        d: '%dd',
+        dd: '%dd'
+      }
+    })
+
     return <RootStack />
   }
 }
