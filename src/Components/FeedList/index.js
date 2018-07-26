ff342c6025c4a31fd473f80096f947f70c488adf
Michael Lustig <lustig.dev@gmail.com>
2018-07-26 13:48:56 -0400

    Move functionality from FeedScreen to FeedList / FeedCard; Add like functionality
    Fixes #23, fixes #30

--- src/Components/FeedList/index.js
+++ src/Components/FeedList/index.js
@@ -1,18 +1,115 @@
 import React from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, ActivityIndicator } from 'react-native'
+import firebase from 'react-native-firebase'
 
-import { FeedCard } from '../FeedCard'
+import FeedCard from '../FeedCard'
+import styles from './FeedListStyles'
+import { Colors } from '../../Themes'
 
-export const FeedList = props => {
-  return (
-    <FlatList
-      extraData={props.extraData}
-      data={props.posts}
-      renderItem={this.renderItem}
-    />
-  )
-}
+export default class FeedList extends React.Component {
+  componentWillMount() {
+    const { uid } = firebase.auth().currentUser
 
-renderItem = ({ item }) => {
-  return <FeedCard item={item} />
+    const rootRef = firebase.database().ref()
+
+    this.uid = uid
+    this.likedPostsRef = rootRef.child('users/' + uid + '/likes')
+    this.likedPostsUrl = this.likedPostsRef.toString() + '.json?shallow=true'
+    this.postsRef = rootRef.child('posts/')
+
+    this.setState({
+      ...this.state,
+      loading: true,
+      likedPosts: new Map(),
+      posts: []
+    })
+
+    this.loadPosts()
+  }
+
+  loadPosts = () => {
+    let likedKeys
+    // First, we need to get an array of liked posts from the user
+    this.likedPostsRef
+      .once('value', snapshot => {
+        // The existence of a key here constitues a like. The value is unused.
+        return Object.keys(snapshot.val())
+      })
+      .then(snapshot => {
+        likedKeys = Object.keys(snapshot.val())
+        return this.postsRef
+          .orderByChild('reverse_timestamp')
+          .once('value', snap => {
+            return snap.val()
+          })
+      })
+      .then(postsSnap => {
+        const posts = Object.values(postsSnap.val())
+
+        const likedPosts = new Map(likedKeys.map(key => [key, true]))
+
+        this.setState({
+          ...this.state,
+          loading: false,
+          likedPosts: likedPosts,
+          posts: posts
+        })
+      })
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <FeedCard
+        liked={!!this.state.likedPosts.get(item.key)}
+        onLikePressed={this.onLikePressed}
+        item={item}
+      />
+    )
+  }
+
+  renderList = () => {
+    return (
+      <FlatList
+        data={this.state.posts}
+        renderItem={this.renderItem}
+        extraData={this.state}
+        keyExtractor={this.keyExtractor}
+      />
+    )
+  }
+
+  renderLoading = () => {
+    return (
+      <ActivityIndicator
+        style={styles.loading}
+        size="large"
+        color={Colors.crimson}
+      />
+    )
+  }
+
+  render() {
+    return this.state.loading ? this.renderLoading() : this.renderList()
+  }
+
+  keyExtractor = item => {
+    return item.key
+  }
+
+  /****************************************************************************
+   ****************************** Interactions ********************************
+   ***************************************************************************/
+  onLikePressed = (key, wasLiked) => {
+    handleLikeToggle = wasLiked
+      ? this.likedPostsRef.child(key).remove()
+      : this.likedPostsRef.child(key).set(!wasLiked)
+
+    handleLikeToggle.then(() => {
+      this.setState(state => {
+        const likedPosts = new Map(state.likedPosts)
+        likedPosts.set(key, !wasLiked)
+        return { likedPosts }
+      })
+    })
+  }
 }
