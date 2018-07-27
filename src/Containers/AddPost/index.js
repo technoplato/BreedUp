923337d5a220851b98479b869e28fd2967d35654
Michael Lustig <lustig.dev@gmail.com>
2018-07-27 16:25:17 -0400

    Add drawer header, move addPost to AddPost placeholder

--- src/Containers/AddPost/index.js
+++ src/Containers/AddPost/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'react-native-firebase'
 
 import styles from './AddPostScreenStyles'
 
@@ -10,5 +11,24 @@ export default class AddPostScreen extends React.Component {
         <Text style={styles.text}>Add Post Screen Placeholder</Text>
       </View>
     )
+  }
+
+  addPost = () => {
+    const ref = firebase
+      .database()
+      .ref()
+      .child('posts')
+      .push()
+
+    ref.set({
+      author: firebase.auth().currentUser.displayName,
+      author_img:
+        'https://firebasestorage.googleapis.com/v0/b/breed-up.appspot.com/o/9sTu43Uw42cGiSMnwroraEDvqfu2%2Fprofile-img?alt=media&token=539cdada-a9b9-41b6-ac22-13cd51698dfd',
+      time_posted: new Date().getTime(),
+      reverse_timestamp: -1 * new Date().getTime(),
+      text:
+        'My dog is the best dog in the entire world and I want this post to be long enough to be a few lines',
+      key: ref.key
+    })
   }
 }
--- src/Navigation/MyCustomSillyDrawerComponent.js
+++ src/Navigation/MyCustomSillyDrawerComponent.js
@@ -4,50 +4,45 @@ import { DrawerItems } from 'react-navigation'
 import firebase from 'react-native-firebase'
 import { Button } from 'react-native-elements'
 
+import { Colors } from '../Themes'
+
 class DrawerContent extends PureComponent {
   render() {
     const { currentUser } = firebase.auth()
 
     return (
       <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 32, paddingTop: 32, alignSelf: 'center' }}>
+        <Text
+          style={{
+            width: '100%',
+            backgroundColor: Colors.crimson,
+            height: 160,
+            paddingTop: 70,
+            color: 'white',
+            textAlign: 'center',
+            alignSelf: 'center',
+            textAlignVertical: 'center',
+            fontSize: 32
+          }}
+        >
           Hi {currentUser && currentUser.displayName}!
         </Text>
+
         <DrawerItems {...this.props} />
 
         <View
           style={{
-            justifyContent: 'space-between',
+            justifyContent: 'flex-end',
             flex: 1,
             paddingVertical: 24
           }}
         >
-          <Button title="Add Post" onPress={this.addPost} />
           <Button title="Log Out" onPress={this.handleLogout} />
         </View>
       </View>
     )
   }
-
-  addPost = () => {
-    const ref = firebase
-      .database()
-      .ref()
-      .child('posts')
-      .push()
-
-    ref.set({
-      author: firebase.auth().currentUser.displayName,
-      author_img:
-        'https://firebasestorage.googleapis.com/v0/b/breed-up.appspot.com/o/9sTu43Uw42cGiSMnwroraEDvqfu2%2Fprofile-img?alt=media&token=539cdada-a9b9-41b6-ac22-13cd51698dfd',
-      time_posted: new Date().getTime(),
-      reverse_timestamp: -1 * new Date().getTime(),
-      text:
-        'My dog is the best dog in the entire world and I want this post to be long enough to be a few lines',
-      key: ref.key
-    })
-  }
-
+  
   handleLogout = () => {
     firebase.auth().signOut()
     this.props.navigation.navigate('SignUp')
