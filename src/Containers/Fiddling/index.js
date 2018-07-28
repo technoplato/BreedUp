d36f2766b41a6c24a3125cbcba17eb3556d097e3
Michael Lustig <lustig.dev@gmail.com>
2018-07-28 16:03:37 -0400

    Use Fiddling to put together Profile placeholder; minimum functionality in place for current and non current user

--- src/Containers/Fiddling/index.js
+++ src/Containers/Fiddling/index.js
@@ -1,59 +1,208 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text, TextInput } from 'react-native'
+import { Button } from 'react-native-elements'
+import firebase from 'react-native-firebase'
+
+import RoundImage from '../../Components/RoundImageView'
+import { Colors } from '../../Themes'
 
 export default class Testing extends React.Component {
-  static navigatorStyle = {
-    drawUnderNavBar: true,
-    navBarTranslucent: true
+  state = {
+    avatarURL: '',
+    username: '',
+    description: '',
+    uid: '',
+    currentUserProfile: true
   }
+  componentWillMount() {
+    const currentUserId =
+      /* TODO: Use firebase.auth().currentUser.uid */ 'IpQAmwyyJrcnUpRq4p549T2M95S2'
+    const profileId = this.props.navigation.getParam(
+      /* TODO: Replace this with reasonable default and handle no ID */ 'uid',
+      'IpQAmwyyJrcnUpRq4p549T2M95S2'
+    )
 
-  constructor(props) {
-    super(props)
+    this.setState({
+      avatarURL: '',
+      username: '',
+      description: '',
+      uid: profileId,
+      currentUserProfile: currentUserId == profileId
+    })
   }
 
   render() {
+    console.log('render', this.state.currentUserProfile)
     return (
-      <View style={styles.container}>
-        <View style={styles.red}>
-          <View style={styles.orange} />
-          <View style={styles.orange} />
+      <View style={styles.screen.container}>
+        {this.header()}
+        {this.dogList()}
+        {this.postsList()}
+        {this.state.currentUserProfile && this.footerButtons()}
+      </View>
+    )
+  }
+
+  header = () => {
+    console.log('header', this.state.currentUserProfile)
+    return (
+      <View style={styles.header.container}>
+        <View style={styles.header.avatarContainer}>
+          <RoundImage
+            size={92}
+            source={{
+              uri:
+                'https://firebasestorage.googleapis.com/v0/b/breed-up.appspot.com/o/IpQAmwyyJrcnUpRq4p549T2M95S2%2Fprofile-img?alt=media&token=aec0140f-e7a7-4cc4-972e-197627e609ee'
+            }}
+          />
         </View>
-        <View style={styles.headerImageContainer}>
-          <View style={styles.fakeImage} />
+        <View style={styles.header.textAndButtonContainer}>
+          <View style={styles.header.topRow}>
+            <View style={styles.header.usernameContainer}>
+              <Text style={styles.header.username}>Username</Text>
+            </View>
+            <View style={styles.header.buttonContainer}>
+              {!this.state.currentUserProfile && (
+                <Button
+                  buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
+                  title="Add to Pack"
+                />
+              )}
+            </View>
+          </View>
+
+          <View style={styles.descriptionContainer}>
+            <TextInput
+              value="Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece of classical "
+              multiline
+              maxLength={120}
+            />
+          </View>
+        </View>
+      </View>
+    )
+  }
+
+  dogList = () => {
+    return (
+      <View style={styles.dogList.container}>
+        <View style={styles.dogList.list}>
+          <Text>Dog List Placholder</Text>
+        </View>
+      </View>
+    )
+  }
+
+  postsList = () => {
+    return (
+      <View style={styles.postList.container}>
+        <View style={styles.postList.list}>
+          <Text>Posts Placholder</Text>
+        </View>
+      </View>
+    )
+  }
+
+  footerButtons = () => {
+    return (
+      <View style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+        <View style={styles.footer.buttonContainer}>
+          <Button
+            title="My Pack"
+            buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
+          />
+          <Button
+            title="Start Event"
+            buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
+          />
         </View>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'blue',
-    flex: 1,
-    justifyContent: 'space-between'
-  },
-  red: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red'
-  },
-  orange: {
-    height: 170,
-    width: 150,
-    backgroundColor: 'orange'
-  },
-  headerImageContainer: {
-    position: 'absolute',
-    minHeight: 24,
-    paddingTop: 24,
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: 'white'
-  },
-  fakeImage: {
-    height: 75,
-    width: 75,
-    backgroundColor: 'black'
-  }
-})
+const styles = {
+  screen: StyleSheet.create({
+    container: {
+      backgroundColor: 'lightskyblue',
+      flex: 1,
+      paddingTop: 24
+    }
+  }),
+
+  header: StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      width: '100%',
+      alignItems: 'center'
+    },
+    avatarContainer: {},
+    textAndButtonContainer: {
+      flex: 1
+    },
+    topRow: {
+      justifyContent: 'space-between',
+      flexDirection: 'row'
+    },
+
+    avatarContainer: {
+      margin: 12,
+      height: 92,
+      width: 92
+    },
+    usernameContainer: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    buttonContainer: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    avatar: {
+      height: 92,
+      borderRadius: 46,
+      width: 92
+    },
+    descriptionContainer: {},
+    username: { fontSize: 22 }
+  }),
+
+  dogList: StyleSheet.create({
+    container: {
+      backgroundColor: 'lightgrey',
+      height: 90,
+      width: '100%'
+    },
+    list: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightgoldenrodyellow'
+    }
+  }),
+
+  postList: StyleSheet.create({
+    container: {
+      backgroundColor: Colors.crimson,
+      flex: 1,
+      padding: 12
+    },
+    list: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightcyan'
+    }
+  }),
+
+  footer: StyleSheet.create({
+    buttonContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor: 'white',
+      padding: 16
+    }
+  })
+}
