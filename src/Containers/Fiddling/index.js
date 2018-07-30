83674f5aad1feede49f901949167f63259a91a19
Michael Lustig <lustig.dev@gmail.com>
2018-07-30 10:50:26 -0400

    Get Profile placeholder to a satisfactory spot

--- src/Containers/Fiddling/index.js
+++ src/Containers/Fiddling/index.js
@@ -4,12 +4,12 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  Dimensions,
-  TouchableHighlight
+  ActivityIndicator
 } from 'react-native'
 import { Button } from 'react-native-elements'
 import firebase from 'react-native-firebase'
 import Modal from 'react-native-modal'
+import _ from 'lodash'
 
 import RoundImage from '../../Components/RoundImageView'
 import { Colors } from '../../Themes'
@@ -21,8 +21,14 @@ export default class Fiddling extends React.Component {
     description: '',
     uid: '',
     currentUserProfile: true,
-    modalVisible: false
+    modalVisible: false,
+    modalSaving: false,
+    modifiedUsername: '',
+    modifiedDescription: '',
+    modifiedAvatarURL: '',
+    hasProfileChanged: false
   }
+
   componentWillMount() {
     const currentUserId =
       /* TODO: Use firebase.auth().currentUser.uid */ 'IpQAmwyyJrcnUpRq4p549T2M95S2'
@@ -33,33 +39,128 @@ export default class Fiddling extends React.Component {
 
     this.setState({
       avatarURL: '',
-      username: '',
-      description: '',
+      username: 'Jenna97',
+      description:
+        'Avid Jeeper. Love bringing my dogs out into the wilderness, hiking, boating and fishing',
       uid: profileId,
-      currentUserProfile: currentUserId == profileId
+      currentUserProfile: currentUserId == profileId,
+
+      modifiedUsername: 'Jenna97',
+      modifiedDescription:
+        'Avid Jeeper. Love bringing my dogs out into the wilderness, hiking, boating and fishing',
+      modifiedAvatarURL: ''
     })
   }
 
   setModalVisible(visible) {
-    this.setState({ modalVisible: visible })
+    this.setState({
+      modalVisible: visible,
+      saveComplete: !visible
+    })
+  }
+
+  setSaving(saving) {
+    this.setState({ modalSaving: saving })
+  }
+
+  saveData() {
+    // TODO
+  }
+
+  onUsernameChange = newUsername => {
+    const usernameChange = this.state.username !== newUsername
+    this.setState({
+      usernameChange: usernameChange,
+      modifiedUsername: newUsername,
+      hasProfileChanged: this.state.hasProfileChanged || usernameChange
+    })
+  }
+
+  onDescriptionChange = newDescription => {
+    const descriptionChange = this.state.description !== newDescription
+    this.setState({
+      descriptionChange: descriptionChange,
+      modifiedDescription: newDescription,
+      hasProfileChanged: this.state.hasProfileChanged || descriptionChange
+    })
+  }
+
+  modal() {
+    return (
+      <Modal
+        isVisible={this.state.modalVisible && !this.state.saveComplete}
+        onBackdropPress={() => this.setModalVisible(false)}
+        style={{ justifyContent: 'center', alignItems: 'center' }}
+      >
+        <View style={{ width: '100%', height: '100%' }}>
+          <View style={{ padding: 12, backgroundColor: 'orange' }}>
+            <Text>Username</Text>
+            <TextInput
+              maxLength={20}
+              style={{ marginBottom: 24 }}
+              onChangeText={this.onUsernameChange}
+              value={this.state.username}
+            />
+
+            <Text>Description</Text>
+            <TextInput
+              maxLength={120}
+              multiline
+              onChangeText={this.onDescriptionChange}
+              value={this.state.description}
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              paddingBottom: 12,
+              minHeight: 42,
+              backgroundColor: 'grey',
+              justifyContent: 'flex-end'
+            }}
+          >
+            <Button
+              title={this.state.modalSaving ? 'Saving...' : 'Save Info'}
+              loading={this.state.modalSaving}
+              disabled={this.state.modalSaving}
+              onPress={() => {
+                console.table(this.state)
+                const {
+                  hasProfileChanged,
+                  modifiedAvatarURL,
+                  modifiedUsername,
+                  modifiedDescription
+                } = this.state
+                if (hasProfileChanged) {
+                  this.setSaving(true)
+                  this.setModalVisible(true)
+                  setTimeout(() => {
+                    this.setModalVisible(false)
+                    this.setSaving(false)
+                    this.setState({
+                      avatarURL: modifiedAvatarURL,
+                      username: modifiedUsername,
+                      description: modifiedDescription,
+                      hasProfileChanged: false,
+                      usernameChange: false,
+                      descriptionChange: false
+                    })
+                  }, 1000)
+                } else {
+                  this.setModalVisible(false)
+                }
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+    )
   }
 
   render() {
     return (
       <View style={styles.screen.container}>
-        <Modal
-          isVisible={this.state.modalVisible}
-          onBackdropPress={() => this.setModalVisible(false)}
-        >
-          <View
-            style={{
-              alignSelf: 'center',
-              height: 200,
-              width: 300,
-              backgroundColor: 'black'
-            }}
-          />
-        </Modal>
+        {this.modal()}
         {this.header()}
         {this.dogList()}
         {this.postsList()}
@@ -73,6 +174,7 @@ export default class Fiddling extends React.Component {
       <View style={styles.header.container}>
         <View style={styles.header.avatarContainer}>
           <RoundImage
+            onPress={() => alert('Edit photo screen ---- TODO')}
             size={92}
             source={{
               uri:
@@ -87,13 +189,15 @@ export default class Fiddling extends React.Component {
                 onPress={() => this.setModalVisible(true)}
                 style={styles.header.username}
               >
-                Username
+                {this.state.username}
               </Text>
             </View>
             <View style={styles.header.buttonContainer}>
               {!this.state.currentUserProfile && (
                 <Button
-                  buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
+                  buttonStyle={{
+                    backgroundColor: Colors.dogBoneBlue
+                  }}
                   title="Add to Pack"
                 />
               )}
@@ -101,12 +205,9 @@ export default class Fiddling extends React.Component {
           </View>
 
           <View style={styles.descriptionContainer}>
-            <TextInput
-              value="Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a piece of classical "
-              multiline
-              maxLength={120}
-            />
+            <Text onPress={() => this.setModalVisible(true)}>
+              {this.state.description}
+            </Text>
           </View>
         </View>
       </View>
@@ -141,10 +242,6 @@ export default class Fiddling extends React.Component {
             title="My Pack"
             buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
           />
-          <Button
-            title="Start Event"
-            buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
-          />
         </View>
       </View>
     )
@@ -155,7 +252,6 @@ const styles = {
   screen: StyleSheet.create({
     container: {
       backgroundColor: 'lightskyblue',
-
       flex: 1,
       paddingTop: 24
     }
@@ -230,9 +326,19 @@ const styles = {
     buttonContainer: {
       flex: 1,
       flexDirection: 'row',
-      justifyContent: 'space-between',
+      justifyContent: 'center',
       backgroundColor: 'white',
       padding: 16
+    }
+  }),
+
+  modal: StyleSheet.create({
+    bottomButtonContainer: {
+      position: 'absolute',
+      right: 0,
+      flex: 1,
+      minHeight: 42,
+      backgroundColor: 'grey'
     }
   })
 }
--- src/Containers/Profile/index.js
+++ src/Containers/Profile/index.js
@@ -1,67 +1,399 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { Avatar, Button } from 'react-native-elements'
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  ActivityIndicator
+} from 'react-native'
+import { Button } from 'react-native-elements'
 import firebase from 'react-native-firebase'
+import Modal from 'react-native-modal'
 
-import styles from './ProfileScreenStyles'
+import RoundImage from '../../Components/RoundImageView'
+import { Colors } from '../../Themes'
 
-export default class ProfileScreen extends React.Component {
-  constructor(props) {
-    super(props)
+export default class Fiddling extends React.Component {
+  state = {
+    avatarURL: '',
+    username: '',
+    description: '',
+    uid: '',
+    currentUserProfile: true,
+    modalVisible: false,
+    modalSaving: false,
+    modifiedUsername: '',
+    modifiedDescription: '',
+    modifiedAvatarURL: '',
+    hasProfileChanged: false,
+    loading: true
+  }
 
-    this.currentUser = firebase.auth().currentUser
-    this.longPlacholder =
-      'aklsdfjl;aksdfj;alsdfjk;alsdkfj;alsdkfj;alsdkfj;alskdfjl;asdkfj;alskdjf;lasdjf;laskjdf;lakjsdfl;kajsdfl;kjas;ldfkjadsl;fkjals;dfkjas;ldkfjals;dkfjals;dfjadls;fjk;laksdjf;laksjdf;laksdjf'
+  componentWillMount() {
 
-    this.state = {
-      profileURL: '',
-      username: '',
-      description: '',
-      dogs: [],
-      posts: []
+    const profileId = this.props.navigation.getParam(
+      /* TODO: Replace this with reasonable default and handle no ID */ 'uid',
+      'JMToo5lrZzMOxTAX8WHPE7t4t5o1'
+    )
+
+    const { uid, displayName, photoURL } = firebase.auth().currentUser
+
+    const currentUserProfile = uid === profileId
+
+    if (currentUserProfile) {
+      this.setState({
+        uid: profileId,
+        currentUserProfile: currentUserProfile,
+
+        avatarURL: photoURL,
+        username: displayName,
+        description: '',
+
+        modifiedUsername: displayName,
+        modifiedDescription: '',
+        modifiedAvatarURL: photoURL
+        // loading: false
+      })
     }
+
+    this.loadUserProfile()
+  }
+
+  loadUserProfile() {
+    firebase
+      .database()
+      .ref()
+      .child(`users/${this.state.uid}`)
+      .on('child_added', snap => {
+        const { username, description, profileURL } = snap.val()
+        console.log(username, description, profileURL)
+
+        this.setState({
+          avatarURL: profileURL,
+          username: username,
+          description: description,
+
+          loading: false
+        })
+      })
+  }
+
+  setModalVisible(visible) {
+    this.setState({
+      modalVisible: visible,
+      saveComplete: !visible
+    })
+  }
+
+  setSaving(saving) {
+    this.setState({ modalSaving: saving })
+  }
+
+  saveData() {
+    // TODO
+  }
+
+  onUsernameChange = newUsername => {
+    const usernameChange = this.state.username !== newUsername
+    this.setState({
+      usernameChange: usernameChange,
+      modifiedUsername: newUsername,
+      hasProfileChanged: this.state.hasProfileChanged || usernameChange
+    })
+  }
+
+  onDescriptionChange = newDescription => {
+    const descriptionChange = this.state.description !== newDescription
+    this.setState({
+      descriptionChange: descriptionChange,
+      modifiedDescription: newDescription,
+      hasProfileChanged: this.state.hasProfileChanged || descriptionChange
+    })
+  }
+
+  modal() {
+    return (
+      <Modal
+        isVisible={this.state.modalVisible && !this.state.saveComplete}
+        onBackdropPress={() => this.setModalVisible(false)}
+        style={{ justifyContent: 'center', alignItems: 'center' }}
+      >
+        <View style={{ width: '100%', height: '100%' }}>
+          <View style={{ padding: 12, backgroundColor: 'orange' }}>
+            <Text>Username</Text>
+            <TextInput
+              maxLength={20}
+              style={{ marginBottom: 24 }}
+              onChangeText={this.onUsernameChange}
+              value={this.state.username}
+            />
+
+            <Text>Description</Text>
+            <TextInput
+              maxLength={120}
+              multiline
+              onChangeText={this.onDescriptionChange}
+              value={this.state.description}
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              paddingBottom: 12,
+              minHeight: 42,
+              backgroundColor: 'grey',
+              justifyContent: 'flex-end'
+            }}
+          >
+            <Button
+              title={this.state.modalSaving ? 'Saving...' : 'Save Info'}
+              loading={this.state.modalSaving}
+              disabled={this.state.modalSaving}
+              onPress={() => {
+                if (this.state.hasProfileChanged) {
+                  this.saveProfileData()
+                } else {
+                  this.setModalVisible(false)
+                }
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+
+  saveProfileData() {
+    this.setModalVisible(false)
+    this.setSaving(false)
+
+    const {
+      modifiedDescription,
+      modifiedAvatarURL,
+      modifiedUsername
+    } = this.state
+
+    firebase
+      .database()
+      .ref()
+      .child(`users/${this.state.uid}`)
+      .update({
+        description: modifiedDescription,
+        profileURL: modifiedAvatarURL,
+        username: modifiedUsername
+      })
+      .then(() => {
+        this.setState({
+          avatarURL: modifiedAvatarURL,
+          username: modifiedUsername,
+          description: modifiedDescription,
+
+          hasProfileChanged: false,
+          usernameChange: false,
+          descriptionChange: false
+        })
+      })
   }
 
   render() {
+    console.log('render', this.state)
     return (
-      <View style={styles.container}>
-        {this.renderProfileHeader()}
-        {this.renderTheRest()}
+      <View style={styles.screen.container}>
+        {this.modal()}
+        {this.header()}
+        {this.dogList()}
+        {this.postsList()}
+        {this.state.currentUserProfile && this.footerButtons()}
+        {this.state.loading && this.renderLoading()}
       </View>
     )
   }
 
-  renderTheRest = () => {
-    return <View style={{ flex: 1, backgroundColor: 'red' }} />
+  renderLoading() {
+    return (
+      <View
+        style={{
+          backgroundColor: 'white',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'absolute',
+          width: '100%',
+          height: '100%'
+        }}
+      >
+        <ActivityIndicator size="large" color={Colors.crimson} />
+      </View>
+    )
   }
 
-  renderProfileHeader = () => {
-    const { profileURL, username, description } = this.state
-
+  header() {
     return (
-      <View style={styles.profileHeaderContainer}>
-        <View style={styles.avatarContainer}>
-          <Avatar
-            rounded
-            size={700}
+      <View style={styles.header.container}>
+        <View style={styles.header.avatarContainer}>
+          <RoundImage
+            onPress={() => alert('Edit photo screen ---- TODO')}
+            size={92}
             source={{
-              uri:
-                profileURL || 'https://dummyimage.com/60x60/000/fff.jpg&text=1'
+              uri: this.state.avatarURL
             }}
           />
         </View>
-        <View style={styles.textAndButtonContainer}>
-          <View style={styles.usernameAndButtonContainer}>
-            <Text style={styles.username}>
-              {username || 'Placholder username'}
-            </Text>
-            <Button title="Add to Pack" buttonStyle={styles.button} />
+        <View style={styles.header.textAndButtonContainer}>
+          <View style={styles.header.topRow}>
+            <View style={styles.header.usernameContainer}>
+              <Text
+                onPress={() => this.setModalVisible(true)}
+                style={styles.header.username}
+              >
+                {this.state.username}
+              </Text>
+            </View>
+            <View style={styles.header.buttonContainer}>
+              {!this.state.currentUserProfile && (
+                <Button
+                  buttonStyle={{
+                    backgroundColor: Colors.dogBoneBlue
+                  }}
+                  title="Add to Pack"
+                />
+              )}
+            </View>
           </View>
-          <Text style={styles.description}>
-            {description || 'Placholder description'}
-          </Text>
+
+          <View style={styles.descriptionContainer}>
+            <Text onPress={() => this.setModalVisible(true)}>
+              {this.state.description}
+            </Text>
+          </View>
         </View>
       </View>
     )
   }
+
+  dogList() {
+    return (
+      <View style={styles.dogList.container}>
+        <View style={styles.dogList.list}>
+          <Text>Dog List Placeholder</Text>
+        </View>
+      </View>
+    )
+  }
+
+  postsList() {
+    return (
+      <View style={styles.postList.container}>
+        <View style={styles.postList.list}>
+          <Text>Posts</Text>
+        </View>
+      </View>
+    )
+  }
+
+  footerButtons() {
+    return (
+      <View style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+        <View style={styles.footer.buttonContainer}>
+          <Button
+            title="My Pack"
+            buttonStyle={{ backgroundColor: Colors.dogBoneBlue }}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  screen: StyleSheet.create({
+    container: {
+      backgroundColor: 'lightskyblue',
+      flex: 1
+    }
+  }),
+
+  header: StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      width: '100%'
+    },
+    avatarContainer: {},
+    textAndButtonContainer: {
+      flex: 1,
+      paddingRight: 12
+    },
+    topRow: {
+      justifyContent: 'space-between',
+      flexDirection: 'row'
+    },
+
+    avatarContainer: {
+      margin: 12,
+      height: 92,
+      width: 92
+    },
+    usernameContainer: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    buttonContainer: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    avatar: {
+      height: 92,
+      borderRadius: 46,
+      width: 92
+    },
+    descriptionContainer: {},
+    username: { fontSize: 22 }
+  }),
+
+  dogList: StyleSheet.create({
+    container: {
+      backgroundColor: 'lightgrey',
+      height: 90,
+      width: '100%'
+    },
+    list: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightgoldenrodyellow'
+    }
+  }),
+
+  postList: StyleSheet.create({
+    container: {
+      backgroundColor: Colors.crimson,
+      flex: 1,
+      padding: 12
+    },
+    list: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightcyan'
+    }
+  }),
+
+  footer: StyleSheet.create({
+    buttonContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+      padding: 16
+    }
+  }),
+
+  modal: StyleSheet.create({
+    bottomButtonContainer: {
+      position: 'absolute',
+      right: 0,
+      flex: 1,
+      minHeight: 42,
+      backgroundColor: 'grey'
+    }
+  })
 }
