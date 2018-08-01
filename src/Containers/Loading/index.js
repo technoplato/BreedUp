f0690938e540f5b4e0c7c36db59265748b95ca11
Michael Lustig <lustig.dev@gmail.com>
2018-07-31 22:36:15 -0400

    Fix sign out logic

--- src/Containers/Loading/index.js
+++ src/Containers/Loading/index.js
@@ -7,16 +7,9 @@ import styles from './LoadingStyles'
 
 export default class Loading extends React.Component {
   componentDidMount() {
-    this.authStateUnsubscribe = firebase.auth().onAuthStateChanged(user => {
-      console.log('Loading: onAuthStateChanged')
-      this.props.navigation.navigate(user ? 'Profile' : 'SignUp')
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
     })
-  }
-
-  componentWillUnmount() {
-    // TODO - when should the listener for auth be removed? Should it be added to App?
-    console.log('componentWillUnmount')
-    this.authStateUnsubscribe()
   }
 
   render() {
