9e746fe85218be514a68a71f3ad438bc8156c41c
Michael Lustig <lustig.dev@gmail.com>
2018-08-01 12:55:39 -0400

    Rudimentary photo choosing functioning with skeleton

--- src/Containers/AddPost/index.js
+++ src/Containers/AddPost/index.js
@@ -1,15 +1,44 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import firebase from 'react-native-firebase'
+import ImagePicker from 'react-native-image-picker'
+import Modal from 'react-native-modal'
 
 import styles from './AddPostScreenStyles'
 
 export default class AddPostScreen extends React.Component {
+  state = { modalVisible: true }
+  constructor(props) {
+    super(props)
+
+    this.cancelPhoto = this.cancelPhoto.bind(this)
+    this.revealPhotoModal = this.revealPhotoModal.bind(this)
+  }
+
+  revealPhotoModal() {
+    this.setState({ modalVisible: true })
+  }
+
+  componentDidMount() {
+    console.log('alksdfj')
+
+    this.props.navigation.addListener('didFocus', this.revealPhotoModal)
+
+    this.state = { modalVisible: true }
+  }
+
+  cancelPhoto() {
+    this.setState({ modalVisible: false })
+    this.props.navigation.goBack()
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Add Post Screen Placeholder</Text>
-      </View>
+      <Test1
+        navigation={this.props.navigation}
+        isVisible={this.state.modalVisible}
+        cancel={this.cancelPhoto}
+      />
     )
   }
 
@@ -22,8 +51,7 @@ export default class AddPostScreen extends React.Component {
 
     ref.set({
       author: firebase.auth().currentUser.displayName,
-      author_img:
-        'https://firebasestorage.googleapis.com/v0/b/breed-up.appspot.com/o/9sTu43Uw42cGiSMnwroraEDvqfu2%2Fprofile-img?alt=media&token=539cdada-a9b9-41b6-ac22-13cd51698dfd',
+      author_img: firebase.auth().currentUser.profileURL,
       time_posted: new Date().getTime(),
       reverse_timestamp: -1 * new Date().getTime(),
       text:
@@ -32,3 +60,103 @@ export default class AddPostScreen extends React.Component {
     })
   }
 }
+
+class Test1 extends React.Component {
+  state = { modalVisible: true, userIsApproving: false }
+
+  constructor(props) {
+    super(props)
+
+    this.showCamera = this.showCamera.bind(this)
+    this.pickImageFromLibrary = this.pickImageFromLibrary.bind(this)
+  }
+
+  render() {
+    return (
+      <Modal style={{ margin: 0 }} isVisible={this.props.isVisible}>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              flex: 0.55,
+              backgroundColor: 'grey',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Text>{this.state.userIsApproving ? 'Still Image' : 'Camera'}</Text>
+          </View>
+          <View
+            style={{
+              flex: 0.45,
+              backgroundColor: 'pink',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Text>
+              {this.state.userIsApproving ? 'Approve Picture' : 'Take Picture'}
+            </Text>
+            <View
+              style={{
+                padding: 16,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                position: 'absolute',
+                bottom: 0,
+                width: '100%'
+              }}
+            >
+              <Text
+                onPress={() => {
+                  if (this.state.userIsApproving) {
+                    this.showCamera()
+                  } else {
+                    this.pickImageFromLibrary()
+                  }
+                }}
+              >
+                {this.state.userIsApproving ? 'Retake' : 'Library'}
+              </Text>
+              <Text onPress={() => this.props.cancel()}>Cancel</Text>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+
+  showCamera() {
+    this.setState({
+      userIsApproving: false
+    })
+  }
+
+  pickImageFromLibrary() {
+    ImagePicker.launchImageLibrary(null, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker')
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error)
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton)
+      } else {
+        this.setState({
+          uri: response.uri,
+          userIsApproving: true
+        })
+      }
+    })
+  }
+}
+
+class Test2 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Test2</Text>
+      </View>
+    )
+  }
+}
+
+export { Test1, Test2 }
--- src/Navigation/Main/index.js
+++ src/Navigation/Main/index.js
@@ -1,15 +1,24 @@
-import { createBottomTabNavigator } from 'react-navigation'
+import React from 'react'
+import { createBottomTabNavigator, NavigationActions } from 'react-navigation'
 
-import { ProfileStack, AddPostStack } from '../Misc'
+import { ProfileStack, AddPost } from '../Misc'
 
 import FeedStack from '../Feed'
 
 import MyCustomSillyDrawerComponent from '../MyCustomSillyDrawerComponent'
 
-const RootStack = createBottomTabNavigator(
+this.previousRoute = 'Main'
+this.currentRoute = 'Main'
+
+const TabNavigation = createBottomTabNavigator(
   {
     Main: FeedStack,
-    AddPost: AddPostStack,
+    AddPost: {
+      screen: AddPost,
+      navigationOptions: {
+        tabBarVisible: false
+      }
+    },
     Profile: ProfileStack
   },
   {
@@ -18,4 +27,31 @@ const RootStack = createBottomTabNavigator(
   }
 )
 
-export default RootStack
+const defaultGetStateForAction = TabNavigation.router.getStateForAction
+TabNavigation.router.getStateForAction = (action, state) => {
+  switch (action.type) {
+    case 'Navigation/INIT':
+      this.currentRoute = 'Main'
+      this.nextRoute = 'Main'
+      break
+    case 'Navigation/NAVIGATE':
+      this.previousRoute = this.currentRoute
+      this.currentRoute = action.routeName
+      this.nextRoute = action.routeName
+      break
+    case 'Navigation/BACK':
+      this.nextRoute = this.previousRoute
+      this.currentRoute = this.nextRoute
+      this.previousRoute = this.currentRoute
+      const index = state.routes.map(route => route.key).indexOf(this.nextRoute)
+      const newState = {
+        routes: state.routes,
+        index: index
+      }
+      return newState
+  }
+
+  return defaultGetStateForAction(action, state)
+}
+
+export default () => <TabNavigation />
