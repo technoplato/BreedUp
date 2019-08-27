5fea3aaf851ed286a98ea93257f7b930820061a7
lustig <halfjew22@gmail.com>
2019-08-27 15:28:24 -0400

    Add Firebase/Auth

--- android/app/src/main/java/com/breedup/MainApplication.java
+++ android/app/src/main/java/com/breedup/MainApplication.java
@@ -3,6 +3,7 @@ package com.breedup;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
 import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeFirebaseAuthPackage(),
             new ReactNativeFirebaseAppPackage()
       );
     }
