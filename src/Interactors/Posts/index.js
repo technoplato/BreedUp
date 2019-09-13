3227082b1be896340b880ad9b0f3c936f99175b6
Michael Lustig <lustig.dev@gmail.com>
2019-09-13 11:07:27 -0400

    Dicking around with create post, trying to reduce image size

--- src/Interactors/Posts/index.js
+++ src/Interactors/Posts/index.js
@@ -1,3 +1,5 @@
+import { Image } from 'react-native'
+
 import { getFollowersForUser } from '../Users'
 import {
   rootRef,
@@ -8,29 +10,58 @@ import {
   currentUser
 } from '../../Utils/FirebaseUtils'
 
+import firestore from '@react-native-firebase/firestore'
+// import { stat } from 'react-native-fs'
+// import RNFetchBlob from 'rn-fetch-blob'
 /**
  * Utility method for creating a post. Also uploads image for post
  */
 const createPost = async (imageUri, text, dogs) => {
-  const { uid, displayName, photoURL } = currentUser()
+  Image.getSize(imageUri, (width, height) => {
+    console.log('width: ', width)
+    console.log('height: ', height)
+  })
+
+  // try {
+  //   const statResult = await stat(imageUri)
+  //   console.log('file size: ' + statResult.size)
+  //   console.log('stats: ', statResult)
+  // } catch (e) {}
+
+  try {
+    // RNFetchBlob.fs
+    //   .stat(PATH_OF_THE_TARGET)
+    //   .then(stats => {
+    //     console.log(stats)
+    //   })
+    //   .catch(err => {})
+  } catch (e) {}
+
+  try {
+  } catch (e) {}
+
+  await new Promise(res => {})
+
+  // const { uid, displayName, photoURL } = currentUser()
   // First, I need to upload the image
-  const postImgUrl = await uploadImageForPost(imageUri, uid)
-  // Create a reference to where the post is going in order to get a key
-  const newPostRef = postsRef.child(uid).push()
-  // Create the post object
-  const post = {
-    author_username: displayName,
-    author_img_url: photoURL,
-    author_id: uid,
-    dogs: dogs,
-    key: newPostRef.key,
-    text: text,
-    comment_count: 0,
-    post_img: postImgUrl,
-    timestamp: new Date().getTime(),
-    view_count: 0
-  }
-  return post
+  // const postImgUrl = await uploadImageForPost(imageUri, uid)
+  // // Create a reference to where the post is going in order to get a key
+  // const postDoc = firestore()
+  //   .collection('posts')
+  //   .doc()
+  // // Create the post object
+  // const post = {
+  //   author: {
+  //     username: displayName,
+  //     photo: photoURL,
+  //     uid: uid
+  //   },
+  //   dogs: dogs,
+  //   id: postDoc.id,
+  //   text: text,
+  //   postPhoto: postImgUrl
+  // }
+  // return post
 }
 
 /**
@@ -40,7 +71,9 @@ const createPost = async (imageUri, text, dogs) => {
  * Returns post after succesful upload.
  */
 const submitPost = async post => {
-  updatePostsForFollowers(post)
+  return post
+  // TODO Firebase cloud function for this if necessary
+  // updatePostsForFollowers(post)
   const postSnap = await addOrUpdatePost(post)
   return post
 }
@@ -57,10 +90,10 @@ const uploadImageForPost = async (postImgUri, authorId) => {
  * on profile view.
  */
 const addOrUpdatePost = post => {
-  return postsRef
-    .child(post.author_id)
-    .child(post.key)
-    .update(post)
+  return firestore()
+    .collection('posts')
+    .doc(post.id)
+    .set(post)
 }
 
 /**
@@ -74,7 +107,7 @@ const updatePostsForFollowers = async post => {
   // show every post to every user.
   const globalFeedPostSnap = await postToGlobalFeed(post)
 
-  const followers = await getFollowersForUser(post.author_id)
+  const followers = await getFollowersForUser(post.author.uid)
   const fanoutObj = fanoutPost(followers, post)
   return rootRef.update(fanoutObj)
 }
@@ -84,9 +117,9 @@ const updatePostsForFollowers = async post => {
  *
  * Adds post to global feed.
  */
-const postToGlobalFeed = async post => {
-  return await globalFeedRef.child(post.key).set(post)
-}
+// const postToGlobalFeed = async post => {
+//   return await globalFeedRef.child(post.key).set(post)
+// }
 
 /**
  * Returns array of posts made by user.
@@ -97,11 +130,6 @@ const getPosts = async userId => {
   const postsSnap = await postsRef.child(userId).once('value')
   return Object.values(postsSnap.val())
 }
-
-/**
- * Get posts a user has liked
- */
-// TODO
 
 /**
  * Creates an object to update all posts on Firebase for all followers.
