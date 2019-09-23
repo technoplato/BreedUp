0b24697787926a229c0690582a5d47709b49516c
Michael Lustig <lustig.dev@gmail.com>
2019-09-23 16:36:58 -0400

    Caught a bug in prune post

--- src/utilities/prune-post.js
+++ src/utilities/prune-post.js
@@ -1,7 +1,7 @@
 export default post => ({
   ...post,
   liked: post.likes.includes(global.user.uid),
-  disliked: (post.likes || []).includes(global.user.uid),
+  disliked: (post.dislikes || []).includes(global.user.uid),
   dislikes: null,
   likes: null
 })
