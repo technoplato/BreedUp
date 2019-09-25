b12174cd61ff036655d7a938ad13cff8c168594f
Michael Lustig <lustig.dev@gmail.com>
2019-09-25 16:13:36 -0400

    Deleted Firebase posts with null author.photo's

--- src/Navigation/AppNavigation.js
+++ src/Navigation/AppNavigation.js
@@ -3,6 +3,15 @@ import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 
 import Onboarding from './Onboarding'
 import Main from './Main'
+import { View, Image } from 'react-native'
+
+const Foo = () => {
+  return (
+    <View>
+      <Image source={{ uri: '' }} />
+    </View>
+  )
+}
 
 const RootNav = createSwitchNavigator({
   Onboarding: Onboarding,
