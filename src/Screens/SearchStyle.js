9e42702131f5024ea804bd0fb27252767516887d
Michael Lustig <halfjew22@gmail.com>
2019-11-16 14:15:56 -0500

    Remove grey background from search

--- src/Screens/SearchStyle.js
+++ src/Screens/SearchStyle.js
@@ -1,11 +1,10 @@
-import { StyleSheet } from "react-native"
-import Colors from "../Themes/Colors"
+import { StyleSheet } from 'react-native'
+import Colors from '../Themes/Colors'
 
 export default StyleSheet.create({
   screen: {
     flex: 1,
-    marginTop: 32,
-    backgroundColor: Colors.grey
+    marginTop: 32
   },
   main: {
     flex: 1,
